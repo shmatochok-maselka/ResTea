5908f048fd601b38e1ad9b71f18e9576933f7a4c
XTHN9RF <justez4rtz@gmail.com>
2022-10-27 22:55:31 +0300

    creating animated carousel

--- Front-End/src/app/blog-carousel/blog-carousel.component.ts
+++ Front-End/src/app/blog-carousel/blog-carousel.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+interface carouseleImages{
+  imageSrc:string;
+  imageAlt:string;
+}
 
 @Component({
   selector: 'app-blog-carousel',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-carousel.component.css']
 })
 export class BlogCarouselComponent implements OnInit {
-
+  @Input() images: carouseleImages[] = [];
+  @Input() indicators = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectedIndex = 0;
+
+  selectImage(index:number):void{
+    this.selectedIndex = index;
+  }
 }
