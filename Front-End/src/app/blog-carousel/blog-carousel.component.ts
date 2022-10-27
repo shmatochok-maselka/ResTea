2833f92c4f7b528021a7c38ed2f737821329b3b3
XTHN9RF <justez4rtz@gmail.com>
2022-10-27 21:34:34 +0300

    sales-carousel initiated

---
+++ Front-End/src/app/blog-carousel/blog-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-carousel',
+  templateUrl: './blog-carousel.component.html',
+  styleUrls: ['./blog-carousel.component.css']
+})
+export class BlogCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Front-End/src/app/blog-carousel/blog-carousel.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogCarouselComponent } from './blog-carousel.component';
+
+
+
+@NgModule({
+  declarations: [
+    BlogCarouselComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    BlogCarouselComponent
+  ]
+})
+export class BlogCarouselModule { }
