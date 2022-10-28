196d9c3c2cbd3a1d5091981040f08ab50ea62d5a
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2022-10-28 09:42:42 +0300

    db initializer

---
+++ restea/src/main/resources/db_initializer.sql
@@ -0,0 +1,6 @@
+USE restea;
+
+INSERT INTO user VALUES (1, 'Ivan', 'Ivanovich', '2022-10-28', 100, 'test@gmail.com');
+INSERT INTO user VALUES (2, 'Petro', 'Ivanovich', '2022-10-28', 100, 'test@gmail.com');
+INSERT INTO user VALUES (3, 'Dmitriy', 'Ivanovich', '2022-10-28', 100, 'test@gmail.com');
+INSERT INTO user VALUES (4, 'Oleg', 'Ivanovich', '2022-10-28', 100, 'test@gmail.com');
