50ef38f74cdd809cf79af7aa99092a2a3fc4f59b
XTHN9RF <justez4rtz@gmail.com>
2022-11-02 23:40:04 +0200

    assets added to project and about-us text changed

--- Front-End/src/app/main-page/about-us/about-us.module.ts
+++ Front-End/src/app/main-page/about-us/about-us.module.ts
@@ -2,18 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AboutUsComponent} from './about-us.component';
 import {FlexModule, FlexLayoutModule} from "@angular/flex-layout";
-import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     AboutUsComponent
   ],
-    imports: [
-        CommonModule,
-        FlexModule,
-        MatButtonModule,
-    ],
+  imports: [
+    CommonModule,
+    FlexModule,
+  ],
   exports: [
     AboutUsComponent
   ]
--- Front-End/src/app/main-page/blog-carousel/blog-carousel.module.ts
+++ Front-End/src/app/main-page/blog-carousel/blog-carousel.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogCarouselComponent } from './blog-carousel.component';
 import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
-import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     BlogCarouselComponent
   ],
-    imports: [
-        CommonModule,
-        FlexModule,
-        MatButtonModule
-    ],
+  imports: [
+    CommonModule,
+    FlexModule
+  ],
   exports: [
     BlogCarouselComponent
   ]
