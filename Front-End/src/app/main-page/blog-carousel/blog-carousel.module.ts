fe76a7dc662b3a9d8934edf34e05a5aa46905fdc
XTHN9RF <justez4rtz@gmail.com>
2022-11-03 00:24:50 +0200

    product cards added and micro-fixes in elements styles applied

--- Front-End/src/app/main-page/blog-carousel/blog-carousel.module.ts
+++ Front-End/src/app/main-page/blog-carousel/blog-carousel.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogCarouselComponent } from './blog-carousel.component';
 import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     BlogCarouselComponent
   ],
-  imports: [
-    CommonModule,
-    FlexModule
-  ],
+    imports: [
+        CommonModule,
+        FlexModule,
+        MatButtonModule
+    ],
   exports: [
     BlogCarouselComponent
   ]
--- Front-End/src/app/main-page/suggested-items/suggested-items.module.ts
+++ Front-End/src/app/main-page/suggested-items/suggested-items.module.ts
@@ -4,17 +4,19 @@ import { SuggestedItemsComponent } from './suggested-items.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {FlexLayoutModule, FlexModule} from "@angular/flex-layout";
+import {ProductCardModule} from "./product-card/product-card.module";
 
 @NgModule({
   declarations: [
     SuggestedItemsComponent
   ],
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatGridListModule,
-    FlexModule,
-  ],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatGridListModule,
+        FlexModule,
+        ProductCardModule,
+    ],
   exports: [
     SuggestedItemsComponent
   ]
