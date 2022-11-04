e6f3b9744ebec4213815fca64cc756dec5eebd7d
shmatochok-maselka <iryna.kopchak39@gmail.com>
2022-11-04 21:13:16 +0200

    deleted ProductDTO

--- restea/src/main/java/com/example/restea/controller/ProductController.java
+++ restea/src/main/java/com/example/restea/controller/ProductController.java
@@ -1,6 +1,5 @@
 package com.example.restea.controller;
 
-import com.example.restea.dto.ProductDto;
 import com.example.restea.model.Product;
 import com.example.restea.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
