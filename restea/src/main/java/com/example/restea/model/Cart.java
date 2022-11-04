91e4d9c63cc79ef6c4cc13607294e637d8e9bf9e
shmatochok-maselka <iryna.kopchak39@gmail.com>
2022-11-05 00:57:28 +0200

    bug_fix/fixed key id for CartId

--- restea/src/main/java/com/example/restea/model/Cart.java
+++ restea/src/main/java/com/example/restea/model/Cart.java
@@ -13,29 +13,36 @@ import java.io.Serializable;
 @Setter
 @ToString
 public class Cart implements  java.io.Serializable{
+    @OneToOne
+    @JoinColumn(name="cart_user_id", insertable=false, updatable=false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name="cart_product_id", insertable=false, updatable=false)
+    private Product cartProduct;
 
     @EmbeddedId
     private CartId id;
 
-    @Transient
-    public User getUser() {
-        return this.id.getUser();
-    }
-
-    @Transient
-    public void setUser(User user) {
-        this.id.setUser(user);
-    }
-
-    @Transient
-    public Product getCartProduct() {
-        return this.id.getCartProduct();
-    }
-
-    @Transient
-    public void setCartProduct(Product product) {
-        this.id.setCartProduct(product);
-    }
+//    @Transient
+//    public User getUser() {
+//        return this.id.getUser();
+//    }
+//
+//    @Transient
+//    public void setUser(User user) {
+//        this.id.setUser(user);
+//    }
+//
+//    @Transient
+//    public Product getCartProduct() {
+//        return this.id.getCartProduct();
+//    }
+//
+//    @Transient
+//    public void setCartProduct(Product product) {
+//        this.id.setCartProduct(product);
+//    }
 
     @Column(name = "weight")
     private int productWeight;
--- restea/src/main/java/com/example/restea/model/CartId.java
+++ restea/src/main/java/com/example/restea/model/CartId.java
@@ -1,23 +1,14 @@
 package com.example.restea.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import java.io.Serializable;
 
 @Embeddable
-@Getter
-@Setter
 public class CartId implements Serializable {
-    @OneToOne
-    @JoinColumn(name="cart_user_id")
-    private User user;
+    @Column(name = "cart_user_id", nullable=false)
+    private long userId;
 
-    @OneToOne
-    @JoinColumn(name="cart_product_id")
-    private Product cartProduct;
+    @Column(name = "cart_product_id", nullable=false)
+    private long productId;
 }
