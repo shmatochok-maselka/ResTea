98f7fe9f5138689013b60f98cad920a53588384b
Las04ka <75950858+Las04ka@users.noreply.github.com>
2022-11-12 00:49:45 +0200

    Added dynamic routes

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -9,7 +9,7 @@ import {MainPageModule} from "./main-page/main-page.module";
 import {NavBarModule} from "./nav-bar/nav-bar.module";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {AppRoutingModule} from "./app-routing.module";
 import {BlogPostComponent} from './blog/blog-post/blog-post.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatGridListModule} from "@angular/material/grid-list";
