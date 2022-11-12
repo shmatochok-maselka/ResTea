5bb7c349ad9ee22d2f5e293971b0daffaffa74f0
XTHN9RF <justez4rtz@gmail.com>
2022-11-12 11:08:46 +0200

    about-us-page component initiated

---
+++ Front-End/src/app/about-us-page/about-us-page.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutUsPageComponent } from './about-us-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutUsPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AboutUsPageComponent
+  ]
+})
+export class AboutUsPageModule { }
