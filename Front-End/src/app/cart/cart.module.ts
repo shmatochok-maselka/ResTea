f6b1e103c7e43bf5d80c24e56498129684f72489
Sasha <napadijoleksandr@gmail.com>
2022-11-15 22:27:45 +0200

    Created cart-product

--- Front-End/src/app/cart/cart.module.ts
+++ Front-End/src/app/cart/cart.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CartComponent} from "./cart.component";
+import { CartProductComponent } from './cart-product/cart-product.component';
 
 
 
 @NgModule({
-  declarations: [CartComponent],
+  declarations: [CartComponent, CartProductComponent],
   imports: [
     CommonModule
   ],
