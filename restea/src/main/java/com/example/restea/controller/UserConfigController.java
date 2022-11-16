b165aff3d4918f6870a81d3c68a8ae1ab1db3b47
Serhiy Oliyarnik <sergoliarnik@gmail.com>
2022-11-16 14:10:11 +0200

    added documentation to update user method

--- restea/src/main/java/com/example/restea/controller/UserConfigController.java
+++ restea/src/main/java/com/example/restea/controller/UserConfigController.java
@@ -3,6 +3,8 @@ package com.example.restea.controller;
 import com.example.restea.dto.UserDataDto;
 import com.example.restea.dto.UserDto;
 import com.example.restea.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,16 @@ public class UserConfigController {
         this.userService = userService;
     }
 
+
+    /**
+     * Method for updating user.
+     *
+     * @return {@link UserDto} instance.
+     * @author Oliyarnik Serhiy.
+     */
+    @Operation(summary = "Create new user")
     @PutMapping(value = "/update")
+    @SecurityRequirement(name = "Bearer Authentication")
     public ResponseEntity<UserDto> update(@RequestBody UserDataDto userDataDto, Principal principal) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(userService.update(userDataDto, principal));
