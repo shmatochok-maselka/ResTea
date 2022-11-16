5f2971a99855e9b4b0f6ce524c5c06f51ac37838
Las04ka <75950858+Las04ka@users.noreply.github.com>
2022-11-16 02:46:51 +0200

    added guards

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -2,15 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { BlogComponent } from './blog/blog.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';0
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MainPageModule} from "./main-page/main-page.module";
 import {NavBarModule} from "./nav-bar/nav-bar.module";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {AppRoutingModule} from "./app-routing.module";
-import {BlogPostComponent} from './blog/blog-post/blog-post.component';
 import {MatCardModule} from "@angular/material/card";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {MatInputModule} from "@angular/material/input";
@@ -25,6 +23,7 @@ import {ContactsPageModule} from "./contacts-page/contacts-page.module";
 import {LoginModule} from "./login/login.module";
 import {RegistrationModule} from "./registration/registration.module";
 import {JwtModule} from "@auth0/angular-jwt";
+import {BlogModule} from "./blog/blog.module";
 
 export function tokenGetter(){
   return localStorage.getItem("token")
@@ -34,8 +33,6 @@ export function tokenGetter(){
   declarations: [
     AppComponent,
     NavBarComponent,
-    BlogComponent,
-    BlogPostComponent,
   ],
     imports: [
         OneProductModule,
@@ -60,6 +57,7 @@ export function tokenGetter(){
         ContactsPageModule,
         LoginModule,
         RegistrationModule,
+        BlogModule,
         JwtModule.forRoot({
           config:{
             tokenGetter:tokenGetter,
--- Front-End/src/app/blog/blog-post/blog-post.module.ts
+++ Front-End/src/app/blog/blog-post/blog-post.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {BlogPostComponent} from "./blog-post.component";
+import {RouterLink} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
 @NgModule({
   declarations: [BlogPostComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    MatButtonModule
   ],
-  exports:[BlogPostComponent]
+  exports:[BlogPostComponent],
 })
 export class BlogPostModule { }
